a98caeddb43186a000082cb81a27e6784a20e503
Alex Johnson <alex@tablelist.com>
2014-11-07 16:35:55 -0500

    Syntax

--- server.js
+++ server.js
@@ -7,11 +7,9 @@ var express = require("express")
   , bodyParser = require("body-parser")           // Body-parser
   , _ = require("underscore");                    // Underscore.js
 
-var participants = [];
-
 // Server config
 app.set("host", "danabucci.dyndns.org"); 	  // Set host to Bucci's server
-app.set("port", 8080);                     	  // Set Port
+app.set("port", 8080);                     	// Set Port
 app.set("views", __dirname + "/views");  	  // Set /views folder
 app.set("view engine", "jade");          	  // Use Jade for HTML parsing
 
@@ -24,7 +22,7 @@ app.use(bodyParser.json());
 // Global Variables
 global.DV = {};
 global.DV.ENV = process.env.NODE_ENV || 'development';
-global.DV.ENV_DEV = (DV.ENV === 'development') || (DV.ENV === 'local');
+global.DV.ENV_EV = (DV.ENV === 'development') || (DV.ENV === 'local');
 global.DV.ENV_PROD = DV.ENV === 'production';
 
 // Config based on current environment
@@ -64,7 +62,7 @@ app.post("/message", function(request, response) {
     return response.json(400, {error: "Message is invalid"});
   }
 
-  //We also expect the sender's name with the message
+  // We also expect the sender's name with the message
   var name = request.body.name;
 
   // Success
