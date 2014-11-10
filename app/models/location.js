ab521f4810b1a1984bee9768e657f9b828b644f7
Alex Johnson <alex@tablelist.com>
2014-11-10 18:46:12 -0500

    Add base scheme for reference

--- app/models/location.js
+++ app/models/location.js
@@ -1,8 +1,18 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
+// var base_data = {
+//   _id       : { type: String, default: utils.objectId, index: { unique: true } },
+//   modified  : { type: Date, default: Date.now, set: setDate },
+//   created   : { type: Date, default: Date.now, set: setDate, index: true }
+// };
+
 var LocationSchema   = new Schema({
-	name: String
+	userId: String,
+	date:   Number,
+	latitude:  Number,
+	longitude: Number,
 });
 
 module.exports = mongoose.model('Location', LocationSchema);
+
