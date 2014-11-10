560d0d3d9b7e88e69076003a0b7e9fdba84b2de7
Alex Johnson <alex@tablelist.com>
2014-11-10 12:20:03 -0500

    Add support for User Model

--- server.js
+++ server.js
@@ -13,23 +13,37 @@ app.use(bodyParser.json());
 var port     = process.env.PORT || 8080; // set our port
 
 var mongoose   = require('mongoose');
-mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to our database
-var Bear     = require('./app/models/bear');
 
-// ROUTES FOR OUR API
-// =============================================================================
+// Project Params (Data Visualization)
+DV = {};
+
+// Store environment-dependent variables
+DV.config = require('./config/config');
+
+// connect to our database
+mongoose.connect(DV.config.development.connectURL); 
+
+// Database Models
+var Bear     = require('./app/models/bear');
+var User     = require('./app/models/user');
+
+
+/*********************************
+          ROUTES
+ *********************************/
 
 // create our router
 var router = express.Router();
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
-	// do logging
 	console.log('Something is happening.');
 	next();
 });
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+// GET localhost:8080/api
+// test route to make sure everything is working
+
 router.get('/', function(req, res) {
 	res.json({ message: 'hooray! welcome to our api!' });	
 });
@@ -100,6 +114,80 @@ router.route('/bears/:bear_id')
 		Bear.remove({
 			_id: req.params.bear_id
 		}, function(err, bear) {
+			if (err)
+				res.send(err);
+
+			res.json({ message: 'Successfully deleted' });
+		});
+	});
+
+
+// on routes that end in /users
+// ----------------------------------------------------
+router.route('/users')
+
+	// create a user (accessed at POST http://localhost:8080/users)
+	.post(function(req, res) {
+		
+		var user = new User();		// create a new instance of the User model
+		user.name = req.body.name;  // set the users name (comes from the request)
+
+		user.save(function(err) {
+			if (err)
+				res.send(err);
+
+			res.json({ message: 'User created!' });
+		});
+
+		
+	})
+
+	// get all the users (accessed at GET http://localhost:8080/api/users)
+	.get(function(req, res) {
+		User.find(function(err, users) {
+			if (err)
+				res.send(err);
+
+			res.json(users);
+		});
+	});
+
+// on routes that end in /users/:user_id
+// ----------------------------------------------------
+router.route('/users/:user_id')
+
+	// get the user with that id
+	.get(function(req, res) {
+		User.findById(req.params.user_id, function(err, user) {
+			if (err)
+				res.send(err);
+			res.json(user);
+		});
+	})
+
+	// update the user with this id
+	.put(function(req, res) {
+		User.findById(req.params.user_id, function(err, user) {
+
+			if (err)
+				res.send(err);
+
+			user.name = req.body.name;
+			user.save(function(err) {
+				if (err)
+					res.send(err);
+
+				res.json({ message: 'User updated!' });
+			});
+
+		});
+	})
+
+	// delete the user with this id
+	.delete(function(req, res) {
+		User.remove({
+			_id: req.params.user_id
+		}, function(err, user) {
 			if (err)
 				res.send(err);
 
