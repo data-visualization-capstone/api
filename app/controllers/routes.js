814a625422585786ee87a0bca89d4063977c8357
Dana <danabucci@gmail.com>
2014-11-14 17:13:38 -0800

    Moved routes out to separate file, removed Express router, added extra comments and console.logs.

---
+++ app/controllers/routes.js
@@ -0,0 +1,144 @@
+// Make this module available to the server.js file
+module.exports = function(app) {
+
+	// Route for the root
+	// ------------------------------------------------------------------------
+	app.route('/')
+		// Return welcome message for all requests to '/'
+		.all(function(req, res, next) {
+			console.log('Root accessed, sending welcome.');
+			res.json({ message: 'hooray! welcome to our api!' });
+		})
+
+	// Route for /users
+	// ------------------------------------------------------------------------
+	app.route('/users')
+		// create a user (accessed at POST http://localhost:8080/users)
+		.post(function(req, res) {
+			
+			var user = new User();		// create a new instance of the User model
+			user.name = req.body.name;  // set the users name (comes from the request)
+
+			user.save(function(err) {
+				if (err)
+					res.send(err);
+				res.json({ message: 'User created!' });
+			});
+		})
+
+		// get all the users (accessed at GET http://localhost:8080/api/users)
+		.get(function(req, res) {
+			console.log('User list requested.');
+			User.find(function(err, users) {
+				if (err)
+					res.send(err);
+				res.json(users);
+			});
+		});
+
+	// Route for /users/:user_id
+	// ------------------------------------------------------------------------
+	app.route('/users/:user_id')
+		// get the user with that id
+		.get(function(req, res) {
+			console.log('User requested by ID.');
+			User.findById(req.params.user_id, function(err, user) {
+				if (err)
+					res.send(err);
+				res.json(user);
+			});
+		})
+
+		// update the user with this id
+		.put(function(req, res) {
+			User.findById(req.params.user_id, function(err, user) {
+				if (err)
+					res.send(err);
+				user.name = req.body.name;
+				user.save(function(err) {
+					if (err)
+						res.send(err);
+					res.json({ message: 'User updated!' });
+				});
+			});
+		})
+
+		// delete the user with this id
+		// .delete(function(req, res) {
+		// 	User.remove({
+		// 		_id: req.params.user_id
+		// 	}, function(err, user) {
+		// 		if (err)
+		// 			res.send(err);
+		// 		res.json({ message: 'Successfully deleted' });
+		// 	});
+		// });
+
+
+	// Route for /locations
+	// ------------------------------------------------------------------------
+	app.route('/locations')
+		// create a location (accessed at POST http://localhost:8080/locations)
+		.post(function(req, res) {
+
+			var location = new Location();	// create a new instance of the Location model
+			location.name = req.body.name;  // set the locations name (comes from the request)
+
+			location.save(function(err) {
+				if (err)
+					res.send(err);
+				res.json({ message: 'Location created!' });
+			});
+		})
+
+		// get all the locations (accessed at GET http://localhost:8080/api/locations)
+		.get(function(req, res) {
+			console.log('Locations list requested.');
+			Location.find(function(err, locations) {
+				if (err)
+					res.send(err);
+				res.json(locations);
+			});
+		});
+
+	// Route for /locations/:location_id
+	// ------------------------------------------------------------------------
+	app.route('/locations/:location_id')
+
+		// get the location with that id
+		.get(function(req, res) {
+			console.log('Location requested by ID.');
+			Location.findById(req.params.location_id, function(err, location) {
+				if (err)
+					res.send(err);
+				res.json(location);
+			});
+		})
+
+		// update the location with this id
+		.put(function(req, res) {
+			Location.findById(req.params.location_id, function(err, location) {
+				if (err)
+					res.send(err);
+				location.name = req.body.name;
+				location.save(function(err) {
+					if (err)
+						res.send(err);
+					res.json({ message: 'Location updated!' });
+				});
+			});
+		})
+
+		// delete the location with this id
+		// .delete(function(req, res) {
+		// 	Location.remove({
+		// 		_id: req.params.location_id
+		// 	}, function(err, location) {
+		// 		if (err)
+		// 			res.send(err);
+
+		// 		res.json({ message: 'Successfully deleted' });
+		// 	});
+		// });
+
+}
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 // BASE SETUP
 // =============================================================================
 
-// call the packages we need
+// Call the packages we need
 var express    = require('express');
 var bodyParser = require('body-parser');
-var app        = express();
 
-// configure app
+// Launch express
+// TODO: made global for routes.js to see. Potentially refactor, or
+//		 move to dedicated global data file.
+app = express();
+
+// Configure app
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -20,19 +24,25 @@ DV = {};
 DV.config = require('./config/config');
 
 
-// connect to our database
+// Connect to our database
 var mongoose   = require('mongoose');
 mongoose.connect(DV.config.development.connectURL); 
 
+
 // Database Models
-var User     = require('./app/models/user');
-var Location = require('./app/models/location');
+// TODO: these were made global to allow the routes file to see them,
+//		 either figure out if this is ok, or move these variables into
+//		 a dedicated global data file.
+User     = require('./app/models/user');
+Location = require('./app/models/location');
 
+// Pull the routes from app/controllers/routes.js
+require('./app/controllers/routes')(app);
 
-/*********************************
-          ROUTES
- *********************************/
-
+//////////////////////////////////////////////////////////
+// Old code commented out 11/14/14
+// TODO: remove if not necessary
+/*
 // create our router
 var router = express.Router();
 
@@ -48,157 +58,9 @@ router.use(function(req, res, next) {
 router.get('/', function(req, res) {
 	res.json({ message: 'hooray! welcome to our api!' });	
 });
+*/
+//////////////////////////////////////////////////////////
 
-
-// on routes that end in /users
-// ----------------------------------------------------
-router.route('/users')
-
-	// create a user (accessed at POST http://localhost:8080/users)
-	.post(function(req, res) {
-		
-		var user = new User();		// create a new instance of the User model
-		user.name = req.body.name;  // set the users name (comes from the request)
-
-		user.save(function(err) {
-			if (err)
-				res.send(err);
-
-			res.json({ message: 'User created!' });
-		});
-
-		
-	})
-
-	// get all the users (accessed at GET http://localhost:8080/api/users)
-	.get(function(req, res) {
-		User.find(function(err, users) {
-			if (err)
-				res.send(err);
-
-			res.json(users);
-		});
-	});
-
-// on routes that end in /users/:user_id
-// ----------------------------------------------------
-router.route('/users/:user_id')
-
-	// get the user with that id
-	.get(function(req, res) {
-		User.findById(req.params.user_id, function(err, user) {
-			if (err)
-				res.send(err);
-			res.json(user);
-		});
-	})
-
-	// update the user with this id
-	.put(function(req, res) {
-		User.findById(req.params.user_id, function(err, user) {
-
-			if (err)
-				res.send(err);
-
-			user.name = req.body.name;
-			user.save(function(err) {
-				if (err)
-					res.send(err);
-
-				res.json({ message: 'User updated!' });
-			});
-
-		});
-	})
-
-	// delete the user with this id
-	// .delete(function(req, res) {
-	// 	User.remove({
-	// 		_id: req.params.user_id
-	// 	}, function(err, user) {
-	// 		if (err)
-	// 			res.send(err);
-
-	// 		res.json({ message: 'Successfully deleted' });
-	// 	});
-	// });
-
-
-// on routes that end in /locations
-// ----------------------------------------------------
-router.route('/locations')
-
-	// create a location (accessed at POST http://localhost:8080/locations)
-	.post(function(req, res) {
-		
-		var location = new Location();		// create a new instance of the Location model
-		location.name = req.body.name;  // set the locations name (comes from the request)
-
-		location.save(function(err) {
-			if (err)
-				res.send(err);
-
-			res.json({ message: 'Location created!' });
-		});
-
-		
-	})
-
-	// get all the locations (accessed at GET http://localhost:8080/api/locations)
-	.get(function(req, res) {
-		Location.find(function(err, locations) {
-			if (err)
-				res.send(err);
-
-			res.json(locations);
-		});
-	});
-
-// on routes that end in /locations/:location_id
-// ----------------------------------------------------
-router.route('/locations/:location_id')
-
-	// get the location with that id
-	.get(function(req, res) {
-		Location.findById(req.params.location_id, function(err, location) {
-			if (err)
-				res.send(err);
-			res.json(location);
-		});
-	})
-
-	// update the location with this id
-	.put(function(req, res) {
-		Location.findById(req.params.location_id, function(err, location) {
-
-			if (err)
-				res.send(err);
-
-			location.name = req.body.name;
-			location.save(function(err) {
-				if (err)
-					res.send(err);
-
-				res.json({ message: 'Location updated!' });
-			});
-
-		});
-	})
-
-	// delete the location with this id
-	// .delete(function(req, res) {
-	// 	Location.remove({
-	// 		_id: req.params.location_id
-	// 	}, function(err, location) {
-	// 		if (err)
-	// 			res.send(err);
-
-	// 		res.json({ message: 'Successfully deleted' });
-	// 	});
-	// });
-
-// REGISTER OUR ROUTES -------------------------------
-app.use('/api', router);
 
 // START THE SERVER
 // =============================================================================
