6beeb62f685d7427081cc2f951a25f9d149872ac
Dana Bucci <danabucci@gmail.com>
2015-02-15 16:46:48 -0500

    stupid tweets not being defined! controllers/tweet.js line 53

--- app/controllers/tweet.js
+++ app/controllers/tweet.js
@@ -7,34 +7,36 @@ var twitter = new Twitter(DV.config.development.twitter);
 exports.getSearch = function(req, res) {
     console.log('twitter search requested');
 
+    // 4 mile radius around downtown Boston.
     var params = {
         q: "#" + req.params.hash,
-        geocode: "42.350000,-71.060000,2mi",
+        geocode: "42.351252, -71.073808, 4mi",
         count: 50,
     };
 
     twitter.get('search/tweets', params, function(error, tweets, response){
         if (!error) {
         // console.log(tweets.statuses)
+            var acc = [];
 
-        var acc = [];
+            for (var i in tweets.statuses){
+                val = tweets.statuses[i];
 
-        for (var i in tweets.statuses){
-            val = tweets.statuses[i];
+                acc.push({
+                    user       : val.id,
+                    created_at : val.created_at,
+                    message    : val.text,
+                    latitude   : val.coordinates.coordinates[0],
+                    longitude  : val.coordinates.coordinates[1],
+                })
+            }
 
-            acc.push({
-                userId    : val.id,
-                date      : val.created_at,
-                message   : val.text,
-                latitude  : val.coordinates.coordinates[0],
-                longitude : val.coordinates.coordinates[1],
-            })
-        }
+            res = setHeaders(res);
+            res.json(acc);
+            // res.json(tweets.statuses[0].coordinates)
 
-        res = setHeaders(res);
-        res.json(acc);
-        // res.json(tweets.statuses[0].coordinates)
-
+        }else{
+            console.log(error);
         }
     });
 }
@@ -42,25 +44,30 @@ exports.getSearch = function(req, res) {
 exports.getStream = function(req, res) {
     console.log('twitter stream requested')
 
+    // 4 mile radius around downtown Boston.
     var params = {
         track: "#" + req.params.hash,
         geocode: "42.351252, -71.073808, 4mi",
     };
 
-    var tweetEntry = new Location();  // create a new instance of the Location model
+    var tweetEntry = new Tweet();  // create a new instance of the Tweet model
 
     twitter.stream('statuses/filter', params, function(stream){
         stream.on('data', function(tweet) {
             console.log(tweet.text);
             if(tweet.coordinates != null) {
                 console.log(tweet.coordinates);
-                tweetEntry.userId       = tweet.user.name;
+                tweetEntry.user         = tweet.user.name;
+                tweetEntry.created_at   = tweet.created_at;
+                tweetEntry.message      = tweet.message;
                 tweetEntry.latitude     = tweet.coordinates.coordinates[0];
                 tweetEntry.longitude    = tweet.coordinates.coordinates[1];
-                tweetEntry.date         = tweet.created_at;
             }else{
-                tweetEntry.userId       = tweet.user.name;
-                tweetEntry.date         = tweet.created_at;
+                tweetEntry.user         = tweet.user.name;
+                tweetEntry.created_at   = tweet.created_at;
+                tweetEntry.message      = tweet.message;
+                tweetEntry.latitude     = null;
+                tweetEntry.longitude    = null;
                 console.log('no location');
             }
             tweetEntry.save(function(err) {
